4faba6829ceda3f4c3d061c671c09f8fd07dc23d
Vicky Choudhary <67699364+vickychoudharyofficial@users.noreply.github.com>
2020-08-24 00:09:39 +0530

    MLM

---
+++ index.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="generator" content="XYZ v1.0">
+    <title>XYZ | Home</title>
+
+    <!-- CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+        <a class="navbar-brand" href="#">XYZ</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+            </ul>
+            <form class="form-inline mt-2 mt-md-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="User ID" aria-label="User ID">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="button"><i class="fa fa-sign-in"></i> LOGIN</button>
+                &nbsp;<button class="btn btn-outline-light my-2 my-sm-0" type="button"><i class="fa fa-user"></i> REGISTER</button>
+            </form>
+        </div>
+    </nav>
+</header>
+
+<main role="main">
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <svg class="bd-placeholder-img" style="border-color: #0275d8; border-style: solid;" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><image href="images/banner/1.jpg" height="100%" /></svg>
+                <div class="container">
+                    <div class="carousel-caption text-left">
+                        <h1 style="color: darkorange;">Example Example</h1>
+                        <p style="color: darkorange;">This is a demo text. This is a demo text. This is a demo text. This is a demo text.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Join Now</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img" style="border-color: #0275d8; border-style: solid;" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><image href="images/banner/2.jpg" height="100%" /></svg>
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1 style="color: #0275d8;">Example Example</h1>
+                        <p style="color: #0275d8;">This is a demo text. This is a demo text. This is a demo text. This is a demo text.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Join Now</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img" style="border-color: #0275d8; border-style: solid;" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><image href="images/banner/3.jpg" height="100%" /></svg>
+                <div class="container">
+                    <div class="carousel-caption text-right">
+                        <h1 style="color: darkorange;">Example Example</h1>
+                        <p style="color: darkorange;">This is a demo text. This is a demo text. This is a demo text. This is a demo text.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Join Now</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+            <div class="col-lg-4">
+                <svg class="bd-placeholder-img rounded-circle" style="border-color: #0275d8; border-style: solid;" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><image href="images/quotes/1.jpg" height="140" width="140" /></svg>
+                <br><br><p>“The richest people in the world look for and build networks, everyone else looks for work.”<br>– <b style="color: #0275d8;"><i>Robert Kiyosaki</i></b></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+                <svg class="bd-placeholder-img rounded-circle" style="border-color: #0275d8; border-style: solid;" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><image href="images/quotes/2.jpg" height="140" width="140" /></svg>
+                <br><br><p>“It’s all about people. It’s about networking and being nice to people and not burning any bridges.”<br>– <b style="color: #0275d8;"><i>Mike Davidson</i></b></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+                <svg class="bd-placeholder-img rounded-circle" style="border-color: #0275d8; border-style: solid;" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><image href="images/quotes/3.jpg" height="140" width="140" /></svg>
+                <br><br><p>“The currency of real networking is not greed but generosity.”<br>– <b style="color: #0275d8;"><i>Keith Ferrazzi</i></b></p>
+            </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+
+
+        <!-- START THE FEATURETTES -->
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">XYZ Online Services. <span class="text-muted">Let's boost the money</span></h2>
+                <p class="lead">XYZ which started its operations in the year 2020, is a leading direct selling company dealing in world class Anti Readiation Chief. XYZ is constantly growing at the rate of 100% every year. This growth pattern in itself speaks volumes about the quality of the products, the marketing plan and the management that has been able to accomplish such a rewarding and sustainable system.</p>
+            </div>
+            <div class="col-md-5">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" style="border-color: #0275d8; border-style: solid;" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><image href="images/about/1.jpg" height="500" width="500" /></svg>
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7 order-md-2">
+                <h2 class="featurette-heading">XYZ Online Services. <span class="text-muted">Vision & Mission</span></h2>
+                <p class="lead">XYZ Online Services proudly aims to become not only the leading Product Company worldwide in terms of volume and number of accounts within two years. It also aims to offer a wide variety of financial instruments, as well as comprehensive money management services. Company's mission is to deliver institutional-level services to all its customers, both institutional and individual retail investors, by providing innovative proprietary technology, low-cost, regulation, safety of customers funds, comprehensive market research tools, advanced programmes and world-class customer service.</p>
+            </div>
+            <div class="col-md-5 order-md-1">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" style="border-color: #0275d8; border-style: solid;" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><image href="images/about/2.jpg" height="500" width="500" /></svg>
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">XYZ Online Services. <span class="text-muted">Join Us Today.</span></h2>
+                <p class="lead">Why are you waiting still, Join Today and multiply your money. Grab the opportunity now.<br><br><a class="btn btn-lg btn-primary" href="#" role="button">Join Now</a></p>
+            </div>
+            <div class="col-md-5">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" style="border-color: #0275d8; border-style: solid;" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><image href="images/about/3.jpg" height="500" width="500" /></svg>
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <!-- /END THE FEATURETTES -->
+
+    </div><!-- /.container -->
+
+
+    <!-- FOOTER -->
+    <footer class="container">
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2020 XYZ &middot; <a href="#">Privacy</a> &middot; <a href="#">T&Cs</a></p>
+    </footer>
+</main>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="js/index.js"></script>
+</html>
